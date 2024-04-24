d0da091a4203dc3981c72f59b8e73b9848a10818
Krish Desai <krishd160@gmail.com>
2024-04-24 14:38:17 -0400

    got routing working

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -22,7 +22,7 @@ export default function NavBar({ session }: { session: Session | null }) {
         } z-30 transition-all`}
       >
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
-          <Link href="/" className="flex items-center font-display text-2xl">
+          <Link href="/TestFolder" className="flex items-center font-display text-2xl">
             <Image
               src="/logo.png"
               alt="Precedent logo"
---
+++ pages/TestFolder/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+const TestPage: React.FC = () => {
+    return (
+        <div>
+            <p>
+                this is a test
+            </p>
+        </div>
+    )
+}
+
+export default TestPage;
