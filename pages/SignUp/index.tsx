6ea273e85d4d73bdde5e6b5cb063c63124b85e36
Kevaunjh <kevaunjh@gmail.com>
2024-04-25 12:14:45 -0400

    Trying to fix the bar

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -6,25 +6,25 @@ const testimg = "https://png.pngtree.com/png-clipart/20230916/original/pngtree-o
 export default function SignUp() {
     return (
         <div className="bg-[#212630] h-screen flex items-center justify-center">
-        <div className="mx-auto text-center border-2 border-black text-red-300 bg-white min-h-[60vh] max-h-[60vh] max-w-[60vw] min-w-[60vw] rounded-3xl flex">
+        <div className=" text-center border-2 border-black bg-white min-h-[60vh] max-h-[60vh] max-w-[60vw] min-w-[60vw] rounded-3xl flex">
           <img src={testimg} alt="ExampleImage" className="w-[50%]"/>
-          <div className="h-auto max-h-[20%] flex items-center justify-center m-auto ">
-            <form className="mx-auto text-[#191970] text-center my-auto">
-              <div className="p-3 text-lg min-w-[15vw]">
+          <div className=" items-center justify-center w-[50%] h-[59vh] flex mr-[8vw]">
+            <form className=" text-[#191970] text-center ">
+              <div className="p-3 text-lg">
                 <label htmlFor="Fname">Username:</label> <br/>
-                <input type="text" id="username" name="username" className="rounded-xl text-center w-[100%]" placeholder="Username" required/>
+                <input type="text" id="username" name="username" className="rounded-xl text-center " placeholder="Username" required/>
               </div>
               <div className="p-3 text-lg">
                 <label htmlFor="Email">Email:</label> <br/>
-                <input type="email" id="email" name="email" className="rounded-xl text-center w-[100%]" placeholder="Email" required/>
+                <input type="email" id="email" name="email" className="rounded-xl text-center" placeholder="Email" required/>
               </div>
               <div className="p-3 text-lg">
                 <label htmlFor="Password">Password:</label> <br/>
-                <input type="password" id="password" name="password" className="rounded-xl text-center w-[100%]" placeholder="Password" required/>
+                <input type="password" id="password" name="password" className="rounded-xl text-center" placeholder="Password" required/>
               </div>
               <div className="p-3 text-lg">
                 <label htmlFor="Cpassword">Password Confirmation:</label> <br/>
-                <input type="password" id="cpassword" name="cpassword" className="rounded-xl text-center w-[100%]" placeholder="Confirm Password" required/>
+                <input type="password" id="cpassword" name="cpassword" className="rounded-xl text-center " placeholder="Confirm Password" required/>
               </div>
               <button type="submit" className="p-3 text-lg">Login</button>
             </form>
