4bd7e7d601f3c4ab4dd7b591ce213968f53277a3
Krish Desai <krishd160@gmail.com>
2024-04-28 16:22:41 -0400

    Topic page matchup

--- pages/Topics/index.tsx
+++ pages/Topics/index.tsx
@@ -1,25 +1,55 @@
 import React from "react";
+import "./../globals.css";
 import Card from "@/components/home/card";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem } from "@nextui-org/dropdown";
 import Form from "platforms/components/form";
+
 
 export default function TopicsForm() {
     //
     
     return(
-        <div>
-            <form className="">
-                <label>Topic: </label>
-                <input type="text" placeholder="Sports,Gaming,Music, e.t.c"></input>
-
-                <label>Parent Category: </label>
-                <input type='text' placeholder="Subtopics"></input>
-
-                <label>Description: </label>
-                <input type="text" placeholder="Description of the topic..."></input>
-                
-
-            </form>
-        </div>
+        
+        
+        <div className="bg-[#212630] min-h-screen flex items-center justify-center">
+              <div className="container bg-[#161a21] md:w-1/2 p-4 rounded-md shadow-lg">
+                <h1 className="text-xl font-bold text-white mb-4">Create a Post</h1>
+                    <form className="space-y-4">
+                        <div>
+                            <label htmlFor="title" className="block text-sm font-medium text-gray-300">Topic</label>
+                            <input
+                            type="text"
+                            name="title"
+                            id="title"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-700 bg-gray-800 text-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="title" className="block text-sm font-medium text-gray-300">Parent Topic</label>
+                            <input
+                            type="text"
+                            name="title"
+                            id="title"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-700 bg-gray-800 text-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="content" className="block text-sm font-medium text-gray-300">Description</label>
+                            <textarea
+                            name="content"
+                            id="content"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-700 bg-gray-800 text-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                            rows={4}
+                            ></textarea>
+                        </div>
+                        <div className="text-right">
+                            <button type="submit" className="px-4 py-1 font-bold bg-blue-500 text-white rounded-full hover:bg-blue-700">
+                                Post
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        
     )
 }
