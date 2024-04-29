fff84e6566e6a1187f9ae09bb5758b2ba26b4299
ved204 <patelved44@outlook.com>
2024-04-28 20:00:33 -0400

    fix page

--- pages/CreatePost/index.tsx
+++ pages/CreatePost/index.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./../globals.css";
 import Navbar from "./../../components/layout/navbar";
 import TopicsForm from "../Topics";
-
 
 // add selector for threads
 // add tags
@@ -13,56 +12,70 @@ import TopicsForm from "../Topics";
 */
 
 export default function CreatePost() {
-    const [text, setText] = useState<string>('CreatePost');
+  const [text, setText] = useState<string>("CreatePost");
 
-    const changecreation = () => {
-        setText("CreateThread");
-      };
-    return (
-        (
-            
-            <div className="bg-[#212630] min-h-screen flex items-center justify-center">
-                 <Navbar session={null}/>
-            
-            {text === 'CreatePost' ?  (
-              <div className="container bg-[#161a21] md:w-1/2 p-4 rounded-md shadow-lg">
-                
-                <h1 className="text-xl font-bold text-white mb-4">Create a New Post</h1>
-                    <form className="space-y-4">
-                        <div>
-                            <label htmlFor="title" className="block text-sm font-medium text-gray-300">Title</label>
-                            <input
-                            type="text"
-                            name="title"
-                            id="title"
-                            className="mt-1 block w-full px-3 py-2 border border-gray-700 bg-gray-800 text-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                            />
-                        </div>
-                                
-                        <div>
-                            <label htmlFor="content" className="block text-sm font-medium text-gray-300">Content</label>
-                            <textarea
-                            name="content"
-                            id="content"
-                            className="mt-1 block w-full px-3 py-2 border border-gray-700 bg-gray-800 text-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                            rows={4}
-                            ></textarea>
-                        </div>
-                        <div className="text-center">
-                            <button onClick={changecreation} className="px-4 py-1 font-bold bg-blue-500 text-white rounded-full hover:bg-blue-700 ml-36 mr-36">
-                                Create New Topic
-                            </button>
-                            <button type="submit" className="px-4 py-1 font-bold bg-blue-500 text-white rounded-full hover:bg-blue-700  ml-36 mr-36">
-                                Post
-                            </button>
-                        </div>
-                    </form>
-                </div>
+  const changecreation = () => {
+    setText("CreateThread");
+  };
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-[#212630]">
+      <Navbar session={null} />
+      <div className="container rounded-md bg-[#161a21] p-4 shadow-lg md:w-1/2">
+        {text === "CreatePost" ? (
+          <>
+            <h1 className="mb-4 text-xl font-bold text-white">
+              Create a New Post
+            </h1>
+            <form className="space-y-4">
+              <div>
+                <label
+                  htmlFor="title"
+                  className="block text-sm font-medium text-gray-300"
+                >
+                  Title
+                </label>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  className="mt-1 block w-full rounded-md border border-gray-700 bg-gray-800 px-3 py-2 text-white shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                />
+              </div>
 
-            ) : (
-                <TopicsForm />
-            )}
-            </div>
-          )
-    )
+              <div>
+                <label
+                  htmlFor="content"
+                  className="block text-sm font-medium text-gray-300"
+                >
+                  Content
+                </label>
+                <textarea
+                  name="content"
+                  id="content"
+                  className="mt-1 block w-full rounded-md border border-gray-700 bg-gray-800 px-3 py-2 text-white shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                  rows={4}
+                ></textarea>
+              </div>
+              <div className="text-center">
+                <button
+                  onClick={changecreation}
+                  className="ml-36 mr-36 rounded-full bg-blue-500 px-4 py-1 font-bold text-white hover:bg-blue-700"
+                >
+                  Create New Topic
+                </button>
+                <button
+                  type="submit"
+                  className="ml-36 mr-36 rounded-full bg-blue-500 px-4 py-1 font-bold  text-white hover:bg-blue-700"
+                >
+                  Post
+                </button>
+              </div>
+            </form>
+          </>
+        ) : (
+          <TopicsForm />
+        )}
+      </div>
+    </div>
+  );
 }
--- pages/Topics/index.tsx
+++ pages/Topics/index.tsx
@@ -4,47 +4,53 @@ import Card from "@/components/home/card";
 import Form from "platforms/components/form";
 import NavBar from "@/components/layout/navbar";
 
-
 export default function TopicsForm() {
-    //
-    
-    return(
-        
-        <div>
+  //
 
-        <div className="bg-[#212630] min-h-screen flex items-center justify-center">
-              <div className="container bg-[#161a21] md:w-1/2 p-4 rounded-md shadow-lg">
-                <h1 className="text-xl font-bold text-white mb-4">Create a New Topic</h1>
-                    <form className="space-y-4">
-                        <div>
-                            <label htmlFor="title" className="block text-sm font-medium text-gray-300">Topic</label>
-                            <input
-                            placeholder="The topic you'd like to create under the parent topic such as a specific video game or specific musical artist"
-                            type="text"
-                            name="title"
-                            id="title"
-                            className="mt-1 block w-full px-3 py-2 border border-gray-700 bg-gray-800 text-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="title" className="block text-sm font-medium text-gray-300">Parent Topic</label>
-                            <input
-                            placeholder="The main topic that you'd like to create such as Video Games, Photography, or Cooking"
-                            type="text"
-                            name="title"
-                            id="title"
-                            className="mt-1 block w-full px-3 py-2 border border-gray-700 bg-gray-800 text-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                            />
-                        </div>
-                      
-                        <div className="text-right">
-                            <button type="submit" className="px-4 py-1 font-bold bg-blue-500 text-white rounded-full hover:bg-blue-700">
-                                Post
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  return (
+    <>
+      <h1 className="mb-4 text-xl font-bold text-white">Create a New Topic</h1>
+      <form className="space-y-4">
+        <div>
+          <label
+            htmlFor="title"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Topic
+          </label>
+          <input
+            placeholder="The topic you'd like to create under the parent topic such as a specific video game or specific musical artist"
+            type="text"
+            name="title"
+            id="title"
+            className="mt-1 block w-full rounded-md border border-gray-700 bg-gray-800 px-3 py-2 text-white shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+          />
         </div>
-    )
+        <div>
+          <label
+            htmlFor="title"
+            className="block text-sm font-medium text-gray-300"
+          >
+            Parent Topic
+          </label>
+          <input
+            placeholder="The main topic that you'd like to create such as Video Games, Photography, or Cooking"
+            type="text"
+            name="title"
+            id="title"
+            className="mt-1 block w-full rounded-md border border-gray-700 bg-gray-800 px-3 py-2 text-white shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+          />
+        </div>
+
+        <div className="text-right">
+          <button
+            type="submit"
+            className="rounded-full bg-blue-500 px-4 py-1 font-bold text-white hover:bg-blue-700"
+          >
+            Post
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
